f6ea3260c02ff2272ddb17eead702b0b707fc99e
dariol <dariol@users.noreply.github.com>
2009-06-12 01:12:17 +0000

    Initial animation attempt at first game: "Pass the Drink"

---
+++ SkylightGame/src/net/nycjava/skylight/service/AndroidPositionPublicationService.java
@@ -0,0 +1,15 @@
+package net.nycjava.skylight.service;
+
+public class AndroidPositionPublicationService implements PositionPublicationService {
+
+	public void addObserver(PositionObserver anObserver) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean removeObserver(PositionObserver anObserver) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ SkylightGame/src/net/nycjava/skylight/service/CountdownPublicationServiceImpl.java
@@ -0,0 +1,108 @@
+package net.nycjava.skylight.service;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+
+enum CounterStatus {
+	uninitialized, running, stopped, finished
+};
+
+public class CountdownPublicationServiceImpl implements CountdownPublicationService {
+
+	private int duration = 0;
+
+	private int currentCount = 0;
+
+	private boolean stopRequested = false;
+
+	private Set<CountdownObserver> countdownObservers = new HashSet<CountdownObserver>();
+
+	private boolean TIME_IS_UP = false;
+
+	private CounterStatus currentStatus = CounterStatus.uninitialized;
+
+	private Timer countdownTimer;
+
+	public CounterStatus getStatus() {
+		return currentStatus;
+	}
+
+	public void addObserver(CountdownObserver anObserver) {
+		countdownObservers.add(anObserver);
+		if (this.currentStatus == CounterStatus.running) {
+			int remain = getRemainingTime();
+			notifyObservers(remain);
+		}
+	}
+
+	public boolean removeObserver(CountdownObserver anObserver) {
+		final boolean existed = countdownObservers.remove(anObserver);
+		return existed;
+	}
+
+	public void setDuration(int seconds) {
+		duration = seconds;
+	}
+
+	private int getRemainingTime() {
+		int aRemainingTime = this.duration - currentCount;
+		return aRemainingTime;
+	}
+
+	class CountdownTask extends TimerTask {
+		public void run() {
+			if (duration == 0)
+				return;
+			currentStatus = CounterStatus.running;
+			if (currentCount < duration && stopRequested == false) {
+				currentCount = currentCount + 1;
+				notifyObservers(getRemainingTime());
+				System.out.println("start counting: " + currentCount);
+
+			}
+			if (currentCount == duration) {
+				TIME_IS_UP = true;
+				currentStatus = CounterStatus.finished;
+			}
+
+			if (stopRequested) {
+				TIME_IS_UP = false;
+				currentStatus = CounterStatus.stopped;
+			}
+		}
+	}
+
+	public void startCountdown() {
+		if (this.duration == 0) {
+			// should be an assertion here
+			return;
+		}
+
+		else if (this.getStatus() == CounterStatus.running) {
+			// should assert here as well
+			return;
+		}
+		countdownTimer = new Timer();
+		CountdownTask countdownTask = new CountdownTask();
+		long zeroDelay = 0;
+		countdownTimer.scheduleAtFixedRate(countdownTask, zeroDelay, 1000);
+	}
+
+	public void stopCountdown() {
+		stopRequested = true;
+		if (countdownTimer != null) {
+			countdownTimer.cancel();
+		}
+		currentStatus = CounterStatus.stopped;
+	}
+
+	private void notifyObservers(int aRemainingTime) {
+
+		for (CountdownObserver countdownObserver : countdownObservers) {
+			countdownObserver.countdownNotification(aRemainingTime);
+		}
+	}
+
+}
